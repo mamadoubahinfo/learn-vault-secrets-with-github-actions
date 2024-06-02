756cec8c1e14ab64fa681efed3e0570cc7a15738
Tanguy NICOLAS <tanguy-nicolas@outlook.fr>
2024-06-02 22:23:11 +0200

    fix: bad folder declaration

--- src/main.py
+++ src/main.py
@@ -11,7 +11,11 @@ app = FastAPI(
     description="Basic API used by example in Vault learning course. Project for University of Picardie (Amiens, France) - Master's Degree."
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.absolute() / "static"),
+    name="static",
+)
 
 templates = Jinja2Templates(directory="templates")
 
